b76c90e840ab5caaec59d7f2f1f97b3c93005970
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-20 10:14:29 +0700

    add game class and test

---
+++ src/Game/game.js
@@ -0,0 +1,103 @@
+const Snake = require("./snake");
+class Game {
+  constructor(snake = new Snake()) {
+    this.snake = snake;
+    this.score = 0;
+    this.hiScore = 0;
+    this.fruitCoordinate = [0, 0];
+    this.isGameOver = false;
+  }
+
+  #intervalId;
+
+  generateFruitCoordinate(arrayOfBody) {
+    let set = new Set();
+    for (let body of arrayOfBody) {
+      set.add(body.coordinate.join());
+    }
+    // TODO: find better solution other than looping
+    while (true) {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      let coordinate = [x, y];
+      let oldSetSize = set.size;
+      set.add(coordinate.join());
+      let newSetSize = set.size;
+      if (oldSetSize !== newSetSize) {
+        this.fruitCoordinate = coordinate;
+        return;
+      }
+    }
+  }
+  start() {
+    this.#intervalId = setInterval(() => {
+      this.snake.move();
+      if (this.#isHittingWall(this.snake.body[0].coordinate)) {
+        this.#gameover();
+      }
+      if (this.#isHittingItself(this.snake.body)) {
+        this.#gameover();
+      }
+      if (
+        this.fruitCoordinate.join() === this.snake.body[0].coordinate.join()
+      ) {
+        this.snake.addLength();
+        this.incrementScore();
+        this.generateFruitCoordinate(this.snake.body);
+      }
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.#intervalId);
+  }
+
+  reset() {
+    this.snake = new Snake();
+    this.resetScore();
+    this.generateFruitCoordinate(this.snake.body);
+    this.isGameOver = false;
+  }
+
+  restart() {
+    this.stop();
+    this.reset();
+    this.start();
+  }
+
+  incrementScore() {
+    this.score++;
+  }
+
+  resetScore() {
+    if (this.hiScore < this.score) {
+      this.hiScore = this.score;
+    }
+    this.score = 0;
+  }
+
+  #gameover() {
+    this.isGameOver = true;
+  }
+
+  #isHittingWall(coordinate) {
+    if (coordinate[0] > 9 || coordinate[0] < 0) return true;
+    if (coordinate[1] > 9 || coordinate[1] < 0) return true;
+    return false;
+  }
+
+  #isHittingItself(arrayOfBody) {
+    let set = new Set();
+    for (let body of arrayOfBody) {
+      let coordinate = body.coordinate.join(",");
+      set.add(coordinate);
+    }
+
+    if (set.size < arrayOfBody.length) {
+      return true;
+    }
+    return false;
+  }
+}
+
+module.exports = Game;
--- src/__test__/game.test.js
+++ src/__test__/game.test.js
@@ -1,17 +1,72 @@
 const Game = require("../Game/game.js");
 const Snake = require("../Game/snake.js");
 
-it("Game score increment", () => {
-  let game = new Game();
-  game.incrementScore();
-  expect(game.score).toBe(1);
-});
-
 it("Game hi score ", () => {
   let game = new Game();
   game.incrementScore();
   game.incrementScore();
   game.resetScore();
-  expect(game.highestScore).toBe(2);
+  expect(game.hiScore).toBe(2);
   expect(game.score).toBe(0);
+});
+
+it("Game over hitting wall", () => {
+  let snake = new Snake(false);
+  snake.addLength("right", [9, 0]);
+  snake.addLength("right", [8, 0]);
+  snake.addLength("right", [7, 0]);
+  let game = new Game(snake);
+  game.start();
+  expect(game.isGameOver).toBe(true);
+});
+
+it("Game over hitting itself", () => {
+  let snake = new Snake(false);
+  snake.addLength("down", [1, 1]);
+  snake.addLength("left", [2, 1]);
+  snake.addLength("up", [2, 0]);
+  snake.addLength("right", [1, 0]);
+  snake.addLength("right", [0, 0]);
+  let game = new Game(snake);
+  game.start();
+  expect(game.isGameOver).toBe(true);
+});
+
+it("not game over if circling around", () => {
+  let snake = new Snake(false);
+  snake.addLength("down", [1, 1]);
+  snake.addLength("left", [2, 1]);
+  snake.addLength("up", [2, 0]);
+  snake.addLength("right", [1, 0]);
+  let game = new Game(snake);
+  game.start();
+  expect(game.isGameOver).toBe(false);
+});
+
+it("add length when eating fruit", () => {
+  let snake = new Snake(false);
+  snake.addLength("up", [4, 0]);
+  snake.addLength("right", [3, 0]);
+  snake.addLength("right", [2, 0]);
+  let game = new Game(snake);
+  game.fruitCoordinate = [4, 1];
+  game.start();
+  expect(snake.length).toBe(4);
+  expect(snake.body[3].coordinate[0]).toBe(2);
+  expect(snake.body[0].coordinate[1]).toBe(1);
+});
+
+it("doesnt generate fruit inside snake body", () => {
+  let snake = new Snake(false);
+  snake.addLength("right", [4, 0]);
+  snake.addLength("right", [2, 0]);
+  snake.addLength("right", [1, 0]);
+  snake.addLength("right", [0, 0]);
+  snake.addLength("right", [0, 1]);
+  snake.addLength("right", [1, 1]);
+  snake.addLength("right", [2, 1]);
+  snake.addLength("right", [3, 1]);
+  snake.addLength("right", [4, 1]);
+  let game = new Game(snake);
+  game.generateFruitCoordinate(snake.body);
 });
