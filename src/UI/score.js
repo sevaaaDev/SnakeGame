474df56d87021ad6ba072b6e7a333eefe0f848f9
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-24 12:55:53 +0700

    create score class UI

---
+++ src/UI/score.js
@@ -0,0 +1,25 @@
+export class Score {
+  constructor() {}
+
+  render([score, highscore]) {
+    const scoreWrapper = document.querySelector(".score-wrapper");
+    const scoreDisplay = document.createElement("p");
+    const hiScoreDisplay = document.createElement("p");
+    scoreDisplay.classList.add("score");
+    hiScoreDisplay.classList.add("hi-score");
+    scoreDisplay.innerText = score;
+    hiScoreDisplay.innerText = "Hi score: " + highscore;
+    scoreWrapper.append(scoreDisplay);
+    scoreWrapper.append(hiScoreDisplay);
+  }
+
+  updateScore(score) {
+    const scoreDisplay = document.querySelector(".score");
+    scoreDisplay.innerText = score;
+  }
+
+  updateHiScore(hiScore) {
+    const hiScoreDisplay = document.querySelector(".hi-score");
+    hiScoreDisplay.innerText = "Hi Score: " + hiScore;
+  }
+}
