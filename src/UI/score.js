de8d7afdfa3ae4c99aba74ea352e8d8978e7b976
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-25 13:24:39 +0700

    change score.render

--- src/UI/score.js
+++ src/UI/score.js
@@ -3,15 +3,10 @@ export class Score {
   constructor() {}
 
   render([score, highscore]) {
-    const scoreWrapper = document.querySelector(".score-wrapper");
-    const scoreDisplay = document.createElement("p");
-    const hiScoreDisplay = document.createElement("p");
-    scoreDisplay.classList.add("score");
-    hiScoreDisplay.classList.add("hi-score");
+    const scoreDisplay = document.querySelector(".score");
+    const hiScoreDisplay = document.querySelector(".hi-score");
     scoreDisplay.innerText = score;
     hiScoreDisplay.innerText = "Hi score: " + highscore;
-    scoreWrapper.append(scoreDisplay);
-    scoreWrapper.append(hiScoreDisplay);
   }
 
   updateScore(score) {
