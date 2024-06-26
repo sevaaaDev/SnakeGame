5dc3f108c34f0d36f2faab1d0541fabe9e990e1c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-26 13:21:07 +0700

    change mode to prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: path.resolve(__dirname, "src"),
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: "inline-source-map",
+  //devtool: "inline-source-map",
   devServer: {
     static: "./dist",
   },
