5b6e4731fa3d3ebf8b871748a086e9d60d6d380b
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-26 13:19:25 +0700

    update webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -38,10 +38,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
     ],
   },
 };
