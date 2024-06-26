842f48a46d5f260d0784b363d76b942e24a1e9b1
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-06-26 13:17:20 +0700

    webpack load sound

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
       },
 
       {
+        test: /\.wav$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
